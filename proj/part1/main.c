c5d73b0868aae85e723ad1aabfe3f7eeaeef5006
ss6545 <145592593+ss6545@users.noreply.github.com>
2025-10-14 15:03:17 -0400

    Part 1 working

--- proj/part1/main.c
+++ proj/part1/main.c
@@ -11,17 +11,20 @@ IDE Lab05 -
 #include "lab5/uart.h"
 #include "lab5/timers.h"
 #include "lab5/switches.h"
+#include "lab5/uart.h"
+#include "uart_extras.h"
 
 void TIMG0_IRQHandler(void);
 void TIMG6_IRQHandler(void);
 void TIMG12_IRQHandler(void);
 
-
-static bool toggle_sw1 = 0;
+// state = 2 means initial state
+static short toggle_sw1 = 2;
 static bool toggle_LED1 = 0;
-static bool toggle_sw2 = 0;
+static short toggle_sw2 = 2;
 static int color_counter = 0;
 static long int ms_c = 0;		// millisecond counter for LED2
+static short display_ms = 2;	// to ensure ms is displayed only once after clicking sw2
 
 //Switch interrupts - GPIO0 (IIDX: 0) and GPIO1 (IIDX: 1)
 void GROUP1_IRQHandler(void);
@@ -46,13 +49,13 @@ int main() {
 	S2_init_interrupt();
 	UART0_init();
 	//TIMG0 w 0.5Hz freq
-	TIMG0_init(39999,199,GPTIMER_CLKDIV_RATIO_DIV_BY_1);
+	TIMG0_init(40000,199,GPTIMER_CLKDIV_RATIO_DIV_BY_1);
 	//TIMG6 w 0.5Hz freq
 	//TIMG6_init(39999,199,GPTIMER_CLKDIV_RATIO_DIV_BY_8);
 	//TIMG6 w 2 Hz freq
-	TIMG6_init(19999,99,GPTIMER_CLKDIV_RATIO_DIV_BY_8);
+	TIMG6_init(20000,99,GPTIMER_CLKDIV_RATIO_DIV_BY_8);
 	//TIMG12 w 1kHz freq
-	TIMG12_init(19);
+	TIMG12_init(32000);
 	
 	//int numS2presses = 0;
 	
@@ -85,9 +88,11 @@ void TIMG6_IRQHandler(){
 			LED1_set(1);
 		}
 	}
-	else {											// switch is off
+	if (toggle_sw1 == 0) {											// switch is off
 		toggle_LED1 = 0;					// turn LED off
 		LED1_set(0);
+//		// test
+//		UART0_put("Disable TIMG6 after this\r\n\0");
 		TIMG6->COUNTERREGS.CTRCTL &= ~GPTIMER_CTRCTL_EN_MASK;	// stop TIMG6 timer
 	}
 	
@@ -96,17 +101,34 @@ void TIMG6_IRQHandler(){
 void TIMG12_IRQHandler(void) {
 	if(toggle_sw2 == 1){				// if switch2 was pressed to turn it on
 		if(!S2_pressed()) {				// wait till sw2 is released to work with the millisecond counting
-		LED2_set(0);							// turn off previous color
-		color_counter++;					// new LED color
-		if (color_counter == 8){	// wrap around for invalid case
-			color_counter = 1;
+			LED2_set(0);							// turn off previous color
+			color_counter++;					// new LED color
+			if (color_counter == 8){	// wrap around for invalid case
+				color_counter = 1;
+			}
+			LED2_set(color_counter);	// turn on new color on LED2
+			ms_c++;										// increase ms counter		
 		}
-		LED2_set(color_counter);	// turn on new color on LED2
-		
 	}
-	}
-	else {
-		
+	if(toggle_sw2 == 0 && display_ms == 1) {
+		if(!S2_pressed()) {				// wait till sw2 is released to work with the millisecond counting
+			LED2_set(0);							// turn off previous color
+			color_counter = 0;				// color = off
+			UART0_put("Time elapsed: \0");
+			UART0_printDec(ms_c);
+			UART0_put(" ms\r\n\0");
+			ms_c = 0;
+			display_ms = 0;						// we are done displaying and do not need to display again until next cycle
+		}
+		else {										// if the switch ahs not been released yet, continue with sw2 on processes
+			LED2_set(0);							// turn off previous color
+			color_counter++;					// new LED color
+			if (color_counter == 8){	// wrap around for invalid case
+				color_counter = 1;
+			}
+			LED2_set(color_counter);	// turn on new color on LED2
+			ms_c++;										// increase ms counter		
+		}
 	}
 }
 
@@ -116,23 +138,28 @@ void GROUP1_IRQHandler() {
 		//clear interrupt at pin for SW1	
 		GPIOA->CPU_INT.ICLR |= GPIO_CPU_INT_ICLR_DIO18_CLR;
 		// toggle
-		if(toggle_sw1) {		// if on
+		if(toggle_sw1 == 1) {		// if on
+//			// test
+//			UART0_put("Turning switch off\r\n\0");
 			toggle_sw1 = 0;		// turn switch off
 		}
-		else { //if off
+		else if(toggle_sw1 == 0 || toggle_sw1 == 2) { //if off
 			TIMG6->COUNTERREGS.CTRCTL |= GPTIMER_CTRCTL_EN_MASK;	// start (enable) TIMG6 timer
+//			// test
+//			UART0_put("Turning switch on\r\n\0");
 			toggle_sw1 = 1;		// turn switch on
 		}
 		
 	} 
-	if (GPIO_CPU_INT_IIDX_STAT_DIO21) {
+	if ((GPIOB->CPU_INT.IIDX & GPIO_CPU_INT_IIDX_STAT_DIO21) == GPIO_CPU_INT_IIDX_STAT_DIO21) {
 		//clear interrupt at pin for SW2	
 		GPIOB->CPU_INT.ICLR |= GPIO_CPU_INT_ICLR_DIO21_CLR;
-		if (toggle_sw2 == 0) {
+		if (toggle_sw2 == 0 || toggle_sw2 == 2) {
 			toggle_sw2 = 1; // turn on sw2
 		}
-		else {
+		else if (toggle_sw2 == 1) {
 			toggle_sw2 = 0;	// turn off sw2
+			display_ms = 1;
 		}
 	}
 }
