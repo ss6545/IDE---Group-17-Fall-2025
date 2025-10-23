61a947b3d2be5bb38f695df975206006176136b2
ss6545 <145592593+ss6545@users.noreply.github.com>
2025-10-23 15:54:47 -0400

    main.c updated for part 1 all

--- proj/lab6/main.c
+++ proj/lab6/main.c
@@ -14,16 +14,16 @@ IDE Lab05 -
 #include "lab6/camera.h"
 #include "uart_extras.h"
 
-
+void delay(void);
 /*
 brief: keeps LEDs on in order to cycle through colors for LED2rgb with S2 input
 note: only used in main.c
 */
-void delay(void) {
-
+void delay() {
 	volatile long timer = 0;
-	while (timer < 40000) timer+=1;
-	
+	while (timer < 40000){
+		timer+=1;
+	}
 }
 
 int main() {
@@ -36,28 +36,56 @@ int main() {
 	UART0_init();
 	ADC0_init();
 	Camera_init();
-	
-	//->1ST SIGNOFF W OSCOPE CAPTURE
-	//20% duty cycle -------V & 10k Hz Freq calc-> TIMA0 is 32 bit so no prescaler
-	//TIMA0_PWM_init(0,3200,0,20);	
-
-	//->2ND OSCOPE CAPTURE
-	
-	for (;;) {
-	//initialize
-	TIMA0_PWM_init(0,3200,0,0);//PIN,PERIOD,PRESCALER,%DUTY CYCLE	
-	TIMA1_PWM_init(3200,0,20);//PERIOD,PRESCALER,%DUTY CYCLE	
-	//delay for 10ms
-	delay();
-	//change duty cycle
-	TIMA0_PWM_DutyCycle(0,3200,100);
-	
-		
-		
-		
+	while(1) {
+	for(int i=0;i<=100; i++){
+		// need to increase the duty cycle for TIMA0 channel 0 from 0 to 100%
+		TIMA0_PWM_init(0,3200,0,i);
+		// channel 1 needs to be set to 0% for the whole time
+		TIMA1_PWM_init(3200,0,0);
+		UART0_printDec(i);
+		UART0_put("\r\n");
+		delay();	// wait for 10ms after each increase in duty cycle
 	}
 	
+	for(int i=100;i>=0; i--){
+		// need to decrease the duty cycle for TIMA0 channel 0 from 100 to 0%
+		TIMA0_PWM_init(0,3200,0,i);
+		// channel 1 needs to be set to 0% for the whole time
+		TIMA1_PWM_init(3200,0,0);
+		UART0_printDec(i);
+		UART0_put("\r\n");
+		delay();	// wait for 10ms after each increase in duty cycle
+	}
 	
+	for(int i=0;i<=100; i++){
+		// need to increase the duty cycle for TIMA0 channel 0 from 0 to 100%
+		TIMA0_PWM_init(0,3200,0,0);
+		// channel 1 needs to be set to 0% for the whole time
+		TIMA1_PWM_init(3200,0,i);
+		UART0_printDec(i);
+		UART0_put("\r\n");
+		delay();	// wait for 10ms after each increase in duty cycle
+	}
+	
+	for(int i=100;i>=0; i--){
+		// need to decrease the duty cycle for TIMA0 channel 0 from 100 to 0%
+		TIMA0_PWM_init(0,3200,0,0);
+		// channel 1 needs to be set to 0% for the whole time
+		TIMA1_PWM_init(3200,0,i);
+		UART0_printDec(i);
+		UART0_put("\r\n");
+		delay();	// wait for 10ms after each increase in duty cycle
+	}
+}
+	
+	
+//	//20% duty cycle -------V & 10k Hz Freq calc-> TIMA0 is 32 bit so no prescaler
+//	TIMA0_PWM_init(0,3200,0,20);	
+
+//	//20% duty cycle -------V & 10k Hz Freq calc-> TIMA0 is 32 bit so no prescaler
+//	TIMA0_PWM_init(1,3200,0,0);
+
+
 	return 0;
 
 }
