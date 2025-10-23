ea135778c4f994cf0dbad4df7e9bd4c271f8915e
an5420 <90580088+an5420@users.noreply.github.com>
2025-10-23 14:36:39 -0400

    uploaded updated main.c and project file for new timers updates

--- proj/lab6/main.c
+++ proj/lab6/main.c
@@ -14,6 +14,18 @@ IDE Lab05 -
 #include "lab6/camera.h"
 #include "uart_extras.h"
 
+
+/*
+brief: keeps LEDs on in order to cycle through colors for LED2rgb with S2 input
+note: only used in main.c
+*/
+void delay(void) {
+
+	volatile long timer = 0;
+	while (timer < 40000) timer+=1;
+	
+}
+
 int main() {
 	
 	//->set up LED1 and S1
@@ -25,12 +37,27 @@ int main() {
 	ADC0_init();
 	Camera_init();
 	
+	//->1ST SIGNOFF W OSCOPE CAPTURE
 	//20% duty cycle -------V & 10k Hz Freq calc-> TIMA0 is 32 bit so no prescaler
-	TIMA0_PWM_init(0,3200,0,20,GPTIMER_CLKDIV_RATIO_DIV_BY_1);	
+	//TIMA0_PWM_init(0,3200,0,20);	
 
-
-
-
+	//->2ND OSCOPE CAPTURE
+	
+	for (;;) {
+	//initialize
+	TIMA0_PWM_init(0,3200,0,0);//PIN,PERIOD,PRESCALER,%DUTY CYCLE	
+	TIMA1_PWM_init(3200,0,20);//PERIOD,PRESCALER,%DUTY CYCLE	
+	//delay for 10ms
+	delay();
+	//change duty cycle
+	TIMA0_PWM_DutyCycle(0,3200,100);
+	
+		
+		
+		
+	}
+	
+	
 	return 0;
 
 }
